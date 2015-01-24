1a57bf34ca26a596737ad472f7946d42ee4aaf0e
retas <kaaretas@gmail.com>
2015-01-25 00:07:05 +0900

    惜しいとこまで

--- tsdiff.py
+++ tsdiff.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python
+# coding: UTF-8
 
 import os
 import glob
 import datetime
 
-print os.getcwd()
-quit()
+pwd = os.getcwd()
+files = glob.glob(pwd + '/*')
+ran_time = datetime.datetime.now()
 
-files = glob.glob('./*')
 diffsec = 0
 for file in files:
     diffsec += 1
-    createdate_tap = datetime.datetime.now() + datetime.timedelta(seconds=diffsec)
-    updatedate_tap = datetime.datetime.now() + datetime.timedelta(seconds=diffsec)
-    times = (int(createdate_tap.strftime('%s')), int(updatedate_tap.strftime('%s')))
+    old_ctime = datetime.datetime.fromtimestamp(os.stat(file).st_ctime)
+    # Macの場合作成日を新しくするのは反映されないっぽいので1時間古くして秒をづらす
+    new_ctime = old_ctime + datetime.timedelta(hours=-1, seconds=diffsec)
+    new_mdified = ran_time + datetime.timedelta(seconds=diffsec)
+    times = (int(new_ctime.strftime('%s')), int(new_mdified.strftime('%s')))
+    print file
+    print u'>> %s ---> %s' % (old_ctime.isoformat(), new_ctime.isoformat())
+    continue
     os.utime(file, times)
